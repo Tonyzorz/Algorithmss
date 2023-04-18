181eeef0f1025efa04b51c56fd95b1fe469c19f3
Taewon Yoon <Tonyzorz@naver.com>
2023-04-18 12:58:38 +0900

    1. https://www.youtube.com/watch?v=94RC-DsGMLo&list=PLRx0vPvlEmdAghTr5mXQxGpHjWqSz0dgC&index=5 진행중

---
+++ src/dongbinna/binary_search/Main.java
@@ -0,0 +1,50 @@
+package dongbinna.binary_search;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            
+            if (arr[mid] == target) return mid;
+            else if (arr[mid] > target) end = mid - 1;
+            else start = mid + 1;
+        }
+        
+        return -1;
+    }
+    
+    /**
+     * 10 7 
+     * 4 3 5 7 9 11 13 15 17 19
+     * 
+     * 기대 return = 4
+     */
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        // 원소의 개수(n)와 찾고자 하는 값(target)을 입력받기 
+        int n = sc.nextInt();
+        int target = sc.nextInt();
+
+        // 전체 원소 입력받기 
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // 이진 탐색 수행 결과 출력 
+        int result = binarySearch(arr, target, 0, n - 1);
+        if (result == -1) {
+            System.out.println("원소가 존재하지 않습니다.");
+        }
+        else {
+            System.out.println(result + 1);
+        }
+    }
+
+}
