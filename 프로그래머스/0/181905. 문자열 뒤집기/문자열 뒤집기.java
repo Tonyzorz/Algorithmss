e6f60a52ab921fe9cc6d8eeb2293fe0bbfe3232d
Taewon Yoon <tonyzorz@naver.com>
2024-07-10 11:00:43 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.33 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        StringBuffer sb = new StringBuffer();
+
+        char[] charArray = my_string.toCharArray();
+        for (int i = 0; i < Math.ceil((e - s) / (float) 2); i++) {
+            char temp = charArray[s + i];
+            charArray[s + i] = charArray[e - i];
+            charArray[e - i] = temp;
+        }
+
+        for (char c : charArray) {
+            sb.append(c);
+        }
+        
+        return sb.toString();
+    }
+}
