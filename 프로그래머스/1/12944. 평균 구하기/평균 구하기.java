193572e1166638e36edf6345a964bfd1b0f7040e
Taewon Yoon <tonyzorz@naver.com>
2024-09-03 20:49:05 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,11 +1,9 @@
 class Solution {
     public double solution(int[] arr) {
         double answer = 0;
-
         for (int num : arr) {
             answer += num;
         }
-
         
         return answer / arr.length;
     }
