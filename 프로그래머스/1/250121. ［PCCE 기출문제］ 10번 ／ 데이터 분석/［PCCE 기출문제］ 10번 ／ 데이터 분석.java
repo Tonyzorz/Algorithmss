4666ce498cb78a437d4a90394e190bfcead1b9b2
Taewon Yoon <tonyzorz@naver.com>
2024-09-09 10:42:43 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 2.76 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -13,7 +13,6 @@ class Solution {
                 list.add(datum);
             }
         }
-
         list.sort(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
