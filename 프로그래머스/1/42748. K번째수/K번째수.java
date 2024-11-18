67435313f4a7e52901f49f00b99ce798c63e0cf0
Taewon Yoon <tonyzorz@naver.com>
2024-11-18 15:44:43 +0900

    [level 1] Title: K번째수, Time: 0.86 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,31 +1,21 @@
 import java.util.Arrays;
 
 class Solution {
+    
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
 
         for (int i = 0; i < commands.length; i++) {
             int start = commands[i][0] - 1;
-            int end = commands[i][1] - 1;
-            int index = commands[i][2];
+            int end = commands[i][1];
+            int index = commands[i][2] - 1;
 
-            int[] organized = cutAndSort(array, start, end);
-            answer[i] = organized[index - 1];
+            int[] newArray = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(newArray);
+
+            answer[i] = newArray[index];
         }
 
         return answer;
-    }
-
-    public int[] cutAndSort(int[] array, int start, int end) {
-        int[] cutted = new int[end - start + 1];
-
-        int index = 0;
-        for (int i = start; i <= end; i++) {
-            cutted[index] = array[i];
-            index++;
-        }
-        Arrays.sort(cutted);
-
-        return cutted;
     }
 }
