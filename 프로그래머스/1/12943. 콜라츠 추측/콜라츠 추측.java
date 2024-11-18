87f01fdb90a269941fe71e2afc66f2b3c3aad147
Taewon Yoon <tonyzorz@naver.com>
2024-11-18 13:13:21 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -2,16 +2,17 @@ class Solution {
     public int solution(int num) {
         int answer = 0;
 
-        long temp = num;
-        while (temp != 1) {
-
-            temp = (temp % 2 == 0) ? temp / 2 : temp * 3 + 1;
+        while (num > 1) {
             answer++;
 
-            if (answer > 500) {
-                return -1;
+            if (num % 2 == 0) {
+                num /= 2;
+                continue;
             }
+
+            num = num * 3 + 1;
         }
-        return answer;
+
+        return (num == 1) ? answer : -1;
     }
 }
