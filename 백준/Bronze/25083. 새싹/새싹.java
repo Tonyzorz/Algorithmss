4688a440fb3a05d032cff154663743349df08f39
Taewon Yoon <tonyzorz@naver.com>
2026-01-15 22:34:11 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+
+        bw.write("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+        br.close();
+        bw.close();
+    }
+
+}
