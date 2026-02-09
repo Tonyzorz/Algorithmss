47b9d3da0051352201709dc13f57365dc2364a04
Taewon Yoon <tonyzorz@naver.com>
2026-02-09 22:06:27 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 92 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String word = br.readLine();
+        int length = word.length();
+
+        bw.write(length + "");
+        bw.flush();
+        bw.close();
+    }
+}
