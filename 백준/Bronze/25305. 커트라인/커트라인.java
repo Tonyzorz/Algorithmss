8592f9a58418e9b740a2db40e207a057c29f1397
Taewon Yoon <tonyzorz@naver.com>
2026-03-07 00:16:21 +0900

    [Bronze II] Title: 커트라인, Time: 112 ms, Memory: 14452 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,30 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int idx = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int cutoffScore = arr[arr.length - idx];
+        bw.write(cutoffScore + "");
+        bw.flush();
+        bw.close();
+    }
+}
