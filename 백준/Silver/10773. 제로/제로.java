4820fdcb8bc44cf1702b63e4eb7a84aca6ee5c1e
Taewon Yoon <tonyzorz@naver.com>
2026-04-07 20:08:25 +0900

    [Silver IV] Title: 제로, Time: 212 ms, Memory: 23748 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,34 +1,35 @@
 import java.io.*;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Stack<Integer> stack = new Stack<>();
+        // 좌측은 인터페이스로 선언 => 우측에는 구현체, 즉 변경에 용이함
+        // 좌측에 타입 명시화 => 우측에는 자바 7 이상부터 명시할 필요 없음
+        Deque<Integer> stack = new ArrayDeque<>();
+
         int repeat = Integer.parseInt(br.readLine());
 
-        for ( int i = 0; i < repeat; i++) {
+        for (int i = 0; i < repeat; i++) {
             int num = Integer.parseInt(br.readLine());
-            if ( num == 0) {
-                stack.pop();
-                continue;
-            }
 
-            stack.add(num);
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
         }
 
         int total = 0;
-        while ( !stack.isEmpty() ) {
+        while (!stack.isEmpty()) {
             total += stack.pop();
         }
 
         bw.write(total + "");
-        br.close();
+        bw.flush();
         bw.close();
     }
-
 }
